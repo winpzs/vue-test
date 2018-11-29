fa92258a9179340b786de7ee934e7448442a4064
winpzs <winpzs@gmail.com>
2018-11-29 14:51:20 +0800

    OPT

--- src/libs/sip/vue-extends/sip-page.ts
+++ src/libs/sip/vue-extends/sip-page.ts
@@ -1,4 +1,4 @@
-import { SipVueCreated } from "./decorators/sip-vue-lifecycle";
+import { SipVueCreated, SipVueDestroyed } from './decorators/sip-vue-lifecycle';
 import { SipBusinessComponent } from "./sip-component";
 import { SipPageLink } from "./sip-page-link";
 
@@ -17,7 +17,7 @@ function _getLink(id: number): SipPageLink {
         let link = _links[index];
         if (_links.length == 1)
             _links = [];
-        else{
+        else {
             _links.splice(index, 1);
             _checkTimeoutLink();
         }
@@ -25,10 +25,10 @@ function _getLink(id: number): SipPageLink {
     }
     return null;
 }
-function _checkTimeoutLink(){
-    if (_links.length > 0){
+function _checkTimeoutLink() {
+    if (_links.length > 0) {
         let time = new Date().valueOf();
-        _links = _links.filter(function(item){
+        _links = _links.filter(function (item) {
             return item.timeOut > time;
         });
     }
@@ -37,14 +37,14 @@ function _checkTimeoutLink(){
 /**页面业务基础类 */
 export class SipPage extends SipBusinessComponent {
 
-    private $sipPageLink:SipPageLink;
+    private _sip_page_link: SipPageLink;
 
     @SipVueCreated()
-    private $sipPageCreated(){
+    private $sipPageCreated() {
         let query = this.$currentRoute.query;
         let _L = query && query._L;
-        if (_L){
-            this.$sipPageLink = _getLink(_L);
+        if (_L) {
+            this._sip_page_link = _getLink(_L);
         }
     }
 
@@ -52,32 +52,28 @@ export class SipPage extends SipBusinessComponent {
         let root: any = this.$root;
         let link = _createLink();
         query = Object.assign({
-            _L:link.id
+            _L: link.id
         }, query);
         root.$sipHome.sipOpen(this.$vueName, path, query, params, false);
         return link;
     }
 
-    $send(...args:any[]){
-        let link = this.$sipPageLink;
-        this.$sipPageLink && this.$sipPageLink.send(...args);
+    $send(...args: any[]) {
+        let link = this._sip_page_link;
+        this._sip_page_link && this._sip_page_link.send(...args);
     }
 
-    // @SipVueBeforeDestroy()
-    // private $sipDestroyClose(){
-    //     if (this.$sipPageIsClose && this.$sipPageLink) return;
-    //     this.$sipPageIsClose = true;
-    //     this.$send();
-    // }
+    @SipVueDestroyed()
+    private _sip_page_destroy() {
+        this._sip_page_link = null;
+    }
 
-    private $sipPageIsClose:boolean;
-    $close(...args:any[]) {
-        if (this.$sipPageIsClose && this.$sipPageLink) return;
-        this.$sipPageIsClose = true;
+    private _sip_page_closed: boolean;
+    $close(...args: any[]) {
+        if (this._sip_page_closed && this._sip_page_link) return;
+        this._sip_page_closed = true;
         this.$send(...args);
         this.$router.go(-1);
     }
-
-    
 
 }
