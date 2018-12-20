5aef93b7e323e9f1c4f69db4d0f12a55fea253ba
winpzs <winpzs@gmail.com>
2018-12-20 14:25:22 +0800

    OPT

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -22,8 +22,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[name].[chunkhash].js')
+    filename: utils.assetsPath('js/[name].[chunkhash:8].js'),
+    chunkFilename: utils.assetsPath('js/[name].[chunkhash:8].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
